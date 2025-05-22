21204b98068af091e7c868d9ba02df47d7b2de00
swapc740 <swapc740@gmail.com>
2025-05-22 13:20:06 +0530

    code to generate ppt

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 env_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=env_path)
-print("OpenAI API Key:", os.getenv("OPENAI_API_KEY"))
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 app = FastAPI()
